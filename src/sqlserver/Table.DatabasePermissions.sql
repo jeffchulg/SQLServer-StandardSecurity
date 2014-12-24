f2b501c5fb65b675b2e24abdfdef86f02cc10112
jeffchulg <jelias@chu.ulg.ac.be>
2014-12-24 10:59:20 +0100

    Modif trigger I/U pour ajouter les nouveaux SchemaName dans DatabaseSchema Ajout d'une FK vers DatabaseSchema pour intégrité SchemaName

--- src/sqlserver/Table.DatabasePermissions.sql
+++ src/sqlserver/Table.DatabasePermissions.sql
@@ -34,6 +34,10 @@
                                     As previous version wasn't a stable release,
                                     no ALTER for that.
     ----------------------------------------------------------------------------------
+    24/12/2014  Jefferson Elias     Modified I/U triggers so that it creates automatically
+                                    the specified schema if it doesn't exist.
+                                    Added FK_DatabasePermissions_SchemaName
+    ----------------------------------------------------------------------------------
   ==================================================================================
 */
 
@@ -96,6 +100,24 @@ BEGIN
     ON [PRIMARY]
 	
 	PRINT '    Primary Key [UN_DatabasePermissions] created.'
+END
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[security].[FK_DatabasePermissions_SchemaName]') AND parent_object_id = OBJECT_ID(N'[security].[DatabasePermissions]'))
+BEGIN
+    ALTER TABLE [security].[DatabasePermissions]
+        ADD  CONSTRAINT [FK_DatabasePermissions_SchemaName]
+            FOREIGN KEY (
+                [ServerName],[DbName],[SchemaName]
+            )
+        REFERENCES [security].[DatabaseSchemas] ([ServerName],[DbName],[SchemaName])
+	IF @@ERROR = 0
+		PRINT '   Foreign Key [FK_DatabasePermissions_SchemaName] created.'
+	ELSE
+	BEGIN
+		PRINT '   Error while trying to create Foreign Key [FK_SQLMappings_Contacts]'
+		RETURN
+	END
 END
 GO
 
@@ -210,6 +232,67 @@ SET @SQL =  'ALTER TRIGGER [security].[TRG_I_DatabasePermissions]' + CHAR(13) +
             '           and ISNULL(o.[SchemaName],''null'') = ISNULL(i.[SchemaName],''null'')' +CHAR(13) +
             '           and o.[objectname] = i.[objectname]' +CHAR(13) +        
             '           and ISNULL(o.[SubObjectName],''null'') = ISNULL(i.[SubObjectName],''null'')' +CHAR(13) +        
+            + CHAR(13) +
+			'	DECLARE forEachRowCursor' + CHAR(13) + 
+			'	CURSOR FOR' + CHAR(13) + 
+			'		select distinct' + CHAR(13) + 
+			'			ServerName,' + CHAR(13) + 
+			'			DbName,' + CHAR(13) + 
+			'			SchemaName' + CHAR(13) +
+			'		from inserted' + CHAR(13) + 
+            '' + CHAR(13) + 
+			'/**' + CHAR(13) + 
+			' * As SQL Server doesn''t have a FOR EACH ROWS trigger, ' + CHAR(13) +
+			' * and as we don''t merge on this table PRIMARY KEY, ' + CHAR(13) + 
+			' * it is mandatory to use a cursor to loop on each rows!' + CHAR(13) + 
+			' */' + CHAR(13) + 
+			'	DECLARE @currentServer	[VARCHAR](256)' + CHAR(13) + 
+			'	DECLARE @currentDbName	[VARCHAR](64)' + CHAR(13) + 
+			'	DECLARE @currentSchema	[VARCHAR](64)' + CHAR(13) + 
+				'' + CHAR(13) + 
+			'	OPEN forEachRowCursor;' + CHAR(13) + 
+			'	FETCH next from forEachRowCursor ' + CHAR(13) + 
+			'		into @currentServer,@currentDbName,@currentSchema' + CHAR(13) + 
+				'' + CHAR(13) + 
+			'	WHILE @@FETCH_STATUS = 0' + CHAR(13) + 
+			'	BEGIN' + CHAR(13) + 
+						'' + CHAR(13) + 
+			'	     merge [security].DatabaseSchemas s' + CHAR(13) +
+			'		 using ( ' + CHAR(13) + 
+			'			select ' + CHAR(13) + 
+			'				@currentServer  as ServerName,' + CHAR(13) + 
+			'				@currentDbName  as DbName,' + CHAR(13) + 
+			'				@currentSchema  as SchemaName' + CHAR(13) + 
+			'		 ) i' + CHAR(13) + 
+			'	     on  s.[ServerName]  = i.[ServerName]' + CHAR(13) +
+			'	     and s.DbName        = i.DbName' + CHAR(13) +
+			'	     and s.[SchemaName]  = i.[SchemaName]' + CHAR(13) +
+			'/*' + CHAR(13) +
+			'	     WHEN MATCHED then' + CHAR(13) +
+			'	         update set' + CHAR(13) +			
+			'	             isActive = 1' + CHAR(13) +
+			'*/' + CHAR(13) +
+			'	     WHEN NOT MATCHED BY TARGET THEN' + CHAR(13) +
+			'	        insert (' + CHAR(13) +
+			'	             ServerName,' + CHAR(13) +
+			'	             DbName,' + CHAR(13) +
+			'	             SchemaName,' + CHAR(13) +
+			'	             Description,' + CHAR(13) +
+			'	             isActive' + CHAR(13) +
+			'	         )' + CHAR(13) +
+			'	         values (' + CHAR(13) +
+			'	             i.ServerName,' + CHAR(13) +
+			'	             i.DbName,' + CHAR(13) +
+			'	             i.SchemaName,' + CHAR(13) +
+			'	             ''Created automatically from DatabasePermissions table'',' + CHAR(13) +
+			'	             1' + CHAR(13) +
+			'	         )' + CHAR(13) +
+			'	     ;' + CHAR(13) +					
+			'			FETCH next from forEachRowCursor ' + CHAR(13) + 
+			'				into @currentServer,@currentDbName,@currentSchema' + CHAR(13) + 
+			'	END;' + CHAR(13) + 
+			'	CLOSE forEachRowCursor;' + CHAR(13) + 
+			'	DEALLOCATE forEachRowCursor;' + CHAR(13) +            
             'END' ;
 EXEC (@SQL);
 PRINT '    Trigger [security].[TRG_I_DatabasePermissions] altered.'
@@ -248,6 +331,68 @@ SET @SQL =  'ALTER TRIGGER [security].[TRG_U_DatabasePermissions]' + CHAR(13) +
             '           and ISNULL(o.[SchemaName],''null'') = ISNULL(i.[SchemaName],''null'')' +CHAR(13) +
             '           and o.[objectname] = i.[objectname]' +CHAR(13) +        
             '           and ISNULL(o.[SubObjectName],''null'') = ISNULL(i.[SubObjectName],''null'')' +CHAR(13) +     
+            + CHAR(13) +
+			'	DECLARE forEachRowCursor' + CHAR(13) + 
+			'	CURSOR FOR' + CHAR(13) + 
+			'		select distinct' + CHAR(13) + 
+			'			ServerName,' + CHAR(13) + 
+			'			DbName,' + CHAR(13) + 
+			'			SchemaName' + CHAR(13) +
+			'		from inserted' + CHAR(13) + 
+            '       where SchemaName is not null' + CHAR(13) +
+            '' + CHAR(13) + 
+			'/**' + CHAR(13) + 
+			' * As SQL Server doesn''t have a FOR EACH ROWS trigger, ' + CHAR(13) +
+			' * and as we don''t merge on this table PRIMARY KEY, ' + CHAR(13) + 
+			' * it is mandatory to use a cursor to loop on each rows!' + CHAR(13) + 
+			' */' + CHAR(13) + 
+			'	DECLARE @currentServer	[VARCHAR](256)' + CHAR(13) + 
+			'	DECLARE @currentDbName	[VARCHAR](64)' + CHAR(13) + 
+			'	DECLARE @currentSchema	[VARCHAR](64)' + CHAR(13) + 
+				'' + CHAR(13) + 
+			'	OPEN forEachRowCursor;' + CHAR(13) + 
+			'	FETCH next from forEachRowCursor ' + CHAR(13) + 
+			'		into @currentServer,@currentDbName,@currentSchema' + CHAR(13) + 
+				'' + CHAR(13) + 
+			'	WHILE @@FETCH_STATUS = 0' + CHAR(13) + 
+			'	BEGIN' + CHAR(13) + 
+						'' + CHAR(13) + 
+			'	     merge [security].DatabaseSchemas s' + CHAR(13) +
+			'		 using ( ' + CHAR(13) + 
+			'			select ' + CHAR(13) + 
+			'				@currentServer  as ServerName,' + CHAR(13) + 
+			'				@currentDbName  as DbName,' + CHAR(13) + 
+			'				@currentSchema  as SchemaName' + CHAR(13) + 
+			'		 ) i' + CHAR(13) + 
+			'	     on  s.[ServerName]  = i.[ServerName]' + CHAR(13) +
+			'	     and s.DbName        = i.DbName' + CHAR(13) +
+			'	     and s.[SchemaName]  = i.[SchemaName]' + CHAR(13) +
+			'/*' + CHAR(13) +
+			'	     WHEN MATCHED then' + CHAR(13) +
+			'	         update set' + CHAR(13) +			
+			'	             isActive = 1' + CHAR(13) +
+			'*/' + CHAR(13) +
+			'	     WHEN NOT MATCHED BY TARGET THEN' + CHAR(13) +
+			'	        insert (' + CHAR(13) +
+			'	             ServerName,' + CHAR(13) +
+			'	             DbName,' + CHAR(13) +
+			'	             SchemaName,' + CHAR(13) +
+			'	             Description,' + CHAR(13) +
+			'	             isActive' + CHAR(13) +
+			'	         )' + CHAR(13) +
+			'	         values (' + CHAR(13) +
+			'	             i.ServerName,' + CHAR(13) +
+			'	             i.DbName,' + CHAR(13) +
+			'	             i.SchemaName,' + CHAR(13) +
+			'	             ''Created automatically from DatabasePermissions table'',' + CHAR(13) +
+			'	             1' + CHAR(13) +
+			'	         )' + CHAR(13) +
+			'	     ;' + CHAR(13) +					
+			'			FETCH next from forEachRowCursor ' + CHAR(13) + 
+			'				into @currentServer,@currentDbName,@currentSchema' + CHAR(13) + 
+			'	END;' + CHAR(13) + 
+			'	CLOSE forEachRowCursor;' + CHAR(13) + 
+			'	DEALLOCATE forEachRowCursor;' + CHAR(13) +                  
             'END' ;
 EXEC (@SQL);
 PRINT '    Trigger [security].[TRG_U_DatabasePermissions] altered.'

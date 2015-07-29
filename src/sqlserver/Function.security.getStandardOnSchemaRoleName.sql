8a0afe33343f80c9b363c9921350597777642d41
jeffchulg <jelias@chu.ulg.ac.be>
2015-07-29 11:17:37 +0200

    Merge from Database-Level access

---
+++ src/sqlserver/Function.security.getStandardOnSchemaRoleName.sql
@@ -0,0 +1,87 @@
+/*requires Schema.Security.sql*/
+/*requires Table.Security.ApplicationParams.sql*/
+
+
+PRINT '--------------------------------------------------------------------------------------------------------------'
+PRINT 'Function [security].[getStandardOnSchemaRoleName] Creation'
+
+IF  NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[security].[getStandardOnSchemaRoleName]') AND type in (N'FN', N'IF', N'TF', N'FS', N'FT'))
+BEGIN
+    EXECUTE ('CREATE FUNCTION [security].[getStandardOnSchemaRoleName] ( ' +
+            ' @ServerName    varchar(512), ' +
+            ' @DbName    varchar(50) ' +
+            ') ' +
+            'RETURNS VARCHAR(max) ' +
+            'AS ' +
+            'BEGIN ' +
+            '   RETURN ''Not implemented'' ' +
+            'END')
+			
+	PRINT '    Function [security].[getStandardOnSchemaRoleName] created.'
+END
+GO
+
+ALTER Function [security].[getStandardOnSchemaRoleName] (
+    @SchemaName         varchar(64)  = NULL,
+    @StandardRoleName   varchar(64)  = NULL
+)
+RETURNS VARCHAR(256)
+AS
+/*
+ ===================================================================================
+  DESCRIPTION:
+
+ 
+  ARGUMENTS :
+    @LoginName              Name of the login to create
+    @AuthMode               Authentication mode : WINDOWS,SQLSRVR
+    @Passwd                 If @AuthMode = 'SQLSRVR', this parameter defines the password
+                            to set by default
+    @DefaultDatabase        Name of the default database for the given login
+    @isActive               If set to 1, the assignment is active and must be done,
+                            TODO if set to 0, this should be like a REVOKE !    
+    @NoHeader               If set to 1, no header will be displayed in the generated statements
+    @NoDependencyCheckGen   if set to 1, no check for server name, database name and so on are generated
+    @Debug                  If set to 1, then we are in debug mode 
+    
+    
+  REQUIREMENTS:
+ 
+  ==================================================================================
+  BUGS:
+ 
+    BUGID       Fixed   Description
+    ==========  =====   ==========================================================
+    ----------------------------------------------------------------------------------
+  ==================================================================================
+  NOTES:
+  AUTHORS:
+       .   VBO     Vincent Bouquette   (vincent.bouquette@chu.ulg.ac.be)
+       .   BBO     Bernard Bozert      (bernard.bozet@chu.ulg.ac.be)
+       .   JEL     Jefferson Elias     (jelias@chu.ulg.ac.be)
+ 
+  COMPANY: CHU Liege
+  ==================================================================================
+  Revision History
+ 
+    Date        Name        Description
+    ==========  =====       ==========================================================
+    16/04/2015  JEL         Version 0.1.0
+ ===================================================================================
+*/
+BEGIN
+
+    DECLARE @SchemaRoleSep      VARCHAR(64)
+
+    select @SchemaRoleSep = ParamValue
+    from [security].[ApplicationParams]
+    where ParamName = 'Separator4OnSchemaStandardRole' ;
+
+    RETURN @SchemaName + @SchemaRoleSep + @StandardRoleName 
+END
+go	
+
+PRINT '    Function [security].[getStandardOnSchemaRoleName] altered.'
+
+PRINT '--------------------------------------------------------------------------------------------------------------'
+PRINT '' 
---
+++ src/sqlserver/Table.inventory.SQLDatabases.sql
@@ -0,0 +1,224 @@
+/*requires Schema.Inventory.sql*/
+/*requires Table.inventory.SQLInstances.sql*/
+
+/**
+  ==================================================================================
+    DESCRIPTION
+        Creation of the [inventory].[SQLDatabases] table.
+        
+        
+    ==================================================================================
+  BUGS:
+ 
+    BUGID       Fixed   Description
+    ==========  =====   ==========================================================
+    ----------------------------------------------------------------------------------
+  ==================================================================================
+  Notes :
+ 
+        Exemples :
+        -------
+ 
+  ==================================================================================
+  Revision history
+ 
+    Date        Name                Description
+    ==========  ================    ================================================
+    13/03/2015  Jefferson Elias     Version 0.1.0
+    ----------------------------------------------------------------------------------    
+  ==================================================================================
+*/
+
+PRINT '--------------------------------------------------------------------------------------------------------------'
+PRINT 'Table [inventory].[SQLDatabases] Creation'
+
+IF  NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[inventory].[SQLDatabases]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE [inventory].[SQLDatabases] (
+        ServerName      VARCHAR(256) NOT NULL,
+        DbName          VARCHAR(64)  NOT NULL,
+        isUserDatabase  BIT          NOT NULL,
+        Reason          VARCHAR(MAX) NULL,
+        DbCreationDate  DATETIME     NULL,
+        DbOwner         VARCHAR(128) NOT NULL default 'sa',
+        DbCollation     VARCHAR(128) NULL,
+        RecoveryModel   VARCHAR(16)  NULL,
+        CompatLevel     TINYINT NULL,       
+        CreationDate    DATETIME NOT NULL,
+        LastModified    DATETIME NOT NULL,
+        Comments        VARCHAR(MAX) NULL
+    )
+    ON [PRIMARY];
+    
+    IF @@ERROR = 0
+        PRINT '   Table created.'
+    ELSE
+    BEGIN
+        PRINT '   Error while trying to create table.'
+        RETURN
+    END
+END
+
+IF (OBJECTPROPERTY( OBJECT_ID( '[inventory].[SQLDatabases]' ), 'TableHasPrimaryKey' ) <> 1)
+BEGIN
+    ALTER TABLE [inventory].[SQLDatabases]
+        ADD  CONSTRAINT [PK_SQLDatabases ]
+            PRIMARY KEY (
+                [ServerName],
+                [DbName]
+            )
+    IF @@ERROR = 0
+        PRINT '   Primary Key [PK_SQLDatabases] created.'
+END
+GO
+
+IF  NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[inventory].[DF_SQLDatabases_CreationDate]') AND type = 'D')
+BEGIN
+    ALTER TABLE [inventory].[SQLDatabases]
+        ADD CONSTRAINT [DF_SQLDatabases_CreationDate]
+            DEFAULT (Getdate()) FOR [CreationDate]
+        
+    PRINT '    Constraint [DF_SQLDatabases_CreationDate] created.'
+END
+GO
+
+IF  NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[inventory].[DF_SQLDatabases_LastModified]') AND type = 'D')
+BEGIN
+    ALTER TABLE [inventory].[SQLDatabases]
+        ADD CONSTRAINT [DF_SQLDatabases_LastModified] DEFAULT (Getdate()) FOR [LastModified]
+    
+    PRINT '    Constraint [DF_SQLDatabases_LastModified] created.'
+END
+GO
+
+IF  NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[inventory].[DF_SQLDatabases_ServerName]') AND type = 'D')
+BEGIN
+    ALTER TABLE [inventory].[SQLDatabases]
+        ADD CONSTRAINT [DF_SQLDatabases_ServerName] DEFAULT (@@SERVERNAME) FOR [ServerName]
+    
+    PRINT '    Constraint [DF_SQLDatabases_ServerName] created.'
+END
+GO
+
+IF  NOT EXISTS (SELECT * FROM sys.check_constraints WHERE object_id = OBJECT_ID(N'[inventory].[CK_SQLDatabases_RecoveryModel]') AND parent_object_id = OBJECT_ID(N'[inventory].[SQLDatabases]'))
+BEGIN
+    ALTER TABLE [inventory].[SQLDatabases]
+        WITH CHECK ADD CONSTRAINT [CK_SQLDatabases_RecoveryModel]
+            CHECK (([RecoveryModel] in ('SIMPLE','BULK_LOGGED','FULL')))
+    PRINT '     Constraint [CK_SQLDatabases_RecoveryModel] created.'
+END
+GO
+
+
+DECLARE @SQL VARCHAR(MAX)
+
+IF  NOT EXISTS (SELECT * FROM sys.triggers WHERE object_id = OBJECT_ID(N'[inventory].[TRG_I_SQLDatabases]'))
+BEGIN
+    SET @SQL = 'CREATE TRIGGER [inventory].[TRG_I_SQLDatabases] ' + CHAR(13) +
+               '  ON inventory.SQLDatabases ' + CHAR(13) +
+               '    FOR INSERT ' + CHAR(13) +
+               'AS' + CHAR(13) +
+               'BEGIN' + CHAR(13) +
+               '    DECLARE @a varchar(MAX)' + CHAR(13) +
+               '    select @a = ''123''' + CHAR(13) +
+               'END' + CHAR(13);
+
+    EXEC (@SQL) ;
+    
+    PRINT '    Trigger [inventory].[TRG_I_SQLDatabases] created.'
+END
+
+SET @SQL =  'ALTER TRIGGER [inventory].[TRG_I_SQLDatabases]' + CHAR(13) +
+            '    ON inventory.SQLDatabases' + CHAR(13) +
+            '    FOR INSERT' +CHAR(13) +
+            'AS' + CHAR(13) +
+            'BEGIN' + CHAR(13) +
+            '    UPDATE [inventory].SQLDatabases ' + CHAR(13) +
+            '        SET LastModified = GETDATE()'+CHAR(13) +
+            '        ,   CreationDate = GETDATE() ' + CHAR(13) +
+            -- '         , CASE WHEN (i.DbName in (''master'',''msdb'',''tempdb'') AND (i.Reason is null)) 
+            '    FROM [inventory].SQLDatabases o ' + CHAR(13) +
+            '        INNER JOIN inserted i' +CHAR(13) +
+            '            on o.[ServerName]  = i.[ServerName]' +CHAR(13) +
+            '           and o.DbName        = i.DbName' +CHAR(13) +
+            '' + CHAR(13) + 
+            '   DECLARE forEachRowCursor' + CHAR(13) + 
+            '   CURSOR LOCAL FOR' + CHAR(13) + 
+            '       select distinct ' + CHAR(13) + 
+            '           ServerName' + CHAR(13) + 
+            '       from inserted' + CHAR(13) + 
+            '' + CHAR(13) + 
+            '/**' + CHAR(13) + 
+            ' * As SQL Server doesn''t have a FOR EACH ROWS trigger, ' + CHAR(13) +
+            ' * and as we don''t merge on this table PRIMARY KEY, ' + CHAR(13) + 
+            ' * it is mandatory to use a cursor to loop on each rows!' + CHAR(13) + 
+            ' */' + CHAR(13) + 
+            '   DECLARE @currentServer  [VARCHAR](256)' + CHAR(13) + 
+            '   OPEN forEachRowCursor;' + CHAR(13) + 
+            '   FETCH next from forEachRowCursor ' + CHAR(13) + 
+            '       into @currentServer' + CHAR(13) + 
+                '' + CHAR(13) + 
+            '   WHILE @@FETCH_STATUS = 0' + CHAR(13) + 
+            '   BEGIN' + CHAR(13) + 
+            '        exec [inventory].[ManageSQLInstance] @ServerName = @currentServer ;' + CHAR(13) + 
+            '   END;' + CHAR(13) + 
+            '   CLOSE forEachRowCursor;' + CHAR(13) + 
+            '   DEALLOCATE forEachRowCursor;' + CHAR(13) +                  
+            'END' ;
+EXEC (@SQL);
+PRINT '    Trigger [inventory].[TRG_I_SQLDatabases] altered.'
+
+IF  NOT EXISTS (SELECT * FROM sys.triggers WHERE object_id = OBJECT_ID(N'[inventory].[TRG_U_SQLDatabases]'))
+BEGIN
+    SET @SQL = 'CREATE TRIGGER [inventory].[TRG_U_SQLDatabases] ' + CHAR(13) +
+               '  ON inventory.SQLDatabases ' + CHAR(13) +
+               '    FOR UPDATE ' + CHAR(13) +
+               'AS' + CHAR(13) +
+               'BEGIN' + CHAR(13) +
+               '    DECLARE @a varchar(MAX)' + CHAR(13) +
+               '    select @a = ''123''' + CHAR(13) +
+               'END' + CHAR(13);
+
+    EXEC (@SQL) ;
+    PRINT '    Trigger [inventory].[TRG_U_SQLDatabases] created.'
+END
+
+SET @SQL =  'ALTER TRIGGER [inventory].[TRG_U_SQLDatabases]' + CHAR(13) +
+            '    ON inventory.SQLDatabases' + CHAR(13) +
+            '    FOR UPDATE' +CHAR(13) +
+            'AS' + CHAR(13) +
+            'BEGIN' + CHAR(13) +
+            '    UPDATE [inventory].SQLDatabases ' + CHAR(13) +
+            '        SET LastModified = GETDATE()'+CHAR(13) +
+            '    FROM [inventory].SQLDatabases o ' + CHAR(13) +
+            '        INNER JOIN inserted i' +CHAR(13) +
+            '            on o.[ServerName]  = i.[ServerName]' +CHAR(13) +
+            '           and o.DbName        = i.DbName' +CHAR(13) +
+            '' + CHAR(13) + 
+            '   DECLARE forEachRowCursor' + CHAR(13) + 
+            '   CURSOR LOCAL FOR' + CHAR(13) + 
+            '       select distinct ' + CHAR(13) + 
+            '           ServerName' + CHAR(13) + 
+            '       from inserted' + CHAR(13) + 
+            '' + CHAR(13) + 
+            '/**' + CHAR(13) + 
+            ' * As SQL Server doesn''t have a FOR EACH ROWS trigger, ' + CHAR(13) +
+            ' * and as we don''t merge on this table PRIMARY KEY, ' + CHAR(13) + 
+            ' * it is mandatory to use a cursor to loop on each rows!' + CHAR(13) + 
+            ' */' + CHAR(13) + 
+            '   DECLARE @currentServer  [VARCHAR](256)' + CHAR(13) + 
+            '   OPEN forEachRowCursor;' + CHAR(13) + 
+            '   FETCH next from forEachRowCursor ' + CHAR(13) + 
+            '       into @currentServer' + CHAR(13) + 
+                '' + CHAR(13) + 
+            '   WHILE @@FETCH_STATUS = 0' + CHAR(13) + 
+            '   BEGIN' + CHAR(13) + 
+            '        exec [inventory].[ManageSQLInstance] @ServerName = @currentServer ;' + CHAR(13) + 
+            '   END;' + CHAR(13) + 
+            '   CLOSE forEachRowCursor;' + CHAR(13) + 
+            '   DEALLOCATE forEachRowCursor;' + CHAR(13) +                  
+            'END' ;
+EXEC (@SQL);
+PRINT '    Trigger [inventory].[TRG_U_SQLDatabases] altered.'
+PRINT '--------------------------------------------------------------------------------------------------------------'
+PRINT '' 
